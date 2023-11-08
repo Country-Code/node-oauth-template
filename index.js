18eb35df3364f68ad5d97edffb6bab6e15381c25
SoufianeMoutaouakkil <moutaouakkilsoufiane@gmail.com>
2023-11-08 11:30:21 +0100

    install dotenv

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const env = require("dotenv")
+
+env.config()
 const app = express()
+const APP_ENV = process.env.APP_ENV
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Express app template!')
+    console.log(APP_ENV + " : Just got a request!")
+    res.send(APP_ENV + " : Express app template!")
 })
 app.listen(process.env.PORT || 5000)
