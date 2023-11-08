0122fe99554e4ef94a5bc4a6008899f31ea425d1
SoufianeMoutaouakkil <moutaouakkilsoufiane@gmail.com>
2023-11-08 11:59:09 +0100

    i mongoose and connect to db and remove .env from repo

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const env = require("dotenv")
+const db = require("./config/db")
 
 env.config()
-const app = express()
 const APP_ENV = process.env.APP_ENV
+
+db.connect()
+
+const app = express()
 
 app.all('/', (req, res) => {
     console.log(APP_ENV + " : Just got a request!")
     res.send(APP_ENV + " : Express app template!")
 })
+
 app.listen(process.env.PORT || 5000)
