5054b86f9d443bfc1cddcdb70dadcd5722a89c0b
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2023-11-10 03:29:06 +0100

    setup AuthMiddleware

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 const userController = require("../controllers/UserController");
-
+const authMiddleware = require("../middlewares/AuthMiddleware")
 router.route("/").post(userController.register);
-router.route("/").get(userController.getAll);
-router.route("/profile").get(userController.getProfile);
+router.route("/").get(authMiddleware.isAdmin, userController.getAll);
+router.route("/profile").get(authMiddleware.isAuthenticated, userController.getProfile);
 router.route("/login").post(userController.login);
 
 module.exports = router;
